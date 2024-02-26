730284e79dfead85b0568c7b2ea7aca3c51357c6
nacor taboada <taboadanacor2033@gmail.com>
2024-02-26 12:54:11 -0300

    0.4

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+const ItemListContainer = ({greeting, saludo}) => {
+    return(
+        <div>
+            <h1>{greeting}</h1>
+            <h3>{saludo}</h3>
+        </div>
+    )
+}
+
+    export default ItemListContainer
+    
