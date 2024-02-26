f5a1619ffa600febba1c95eb71080da4f8ff718c
nacor taboada <taboadanacor2033@gmail.com>
2024-02-26 13:07:31 -0300

    0.5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css"
 import Navbar from "./components/navbar/Navbar"
-import ItemListContainer from "./components/itemListcontainer/ItemListContainer";
+import ItemListContainer from './components/itemListcontainer/ItemListContainer';
 function App() {
   const saludo= "hola chicos"
   return (
    <div>
-    <ItemListContainer greeting= "bienvenidos a mi app" saludo={saludo}/>
+    
      <Navbar/>
+     <ItemListContainer greeting= "bienvenidos a mi app" saludo={saludo}/>
    </div>
   );
 }
